d2b59cb86017502e2ca10e63f86f0468e8fbd6d0
DiptoChakrabarty <diptochuck123@gmail.com>
2020-02-08 15:45:50 +0530

    connecting to server

--- ftpcrack.py
+++ ftpcrack.py
@@ -3,4 +3,8 @@ import re
 import sys
 
 #Connect to ftp server
-def connection(ip,passwd):
+def connection(ip,user,passwd):
+
+    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    print("Connecting to ip" + ip  +"with user " + user + "password " +passwd)
