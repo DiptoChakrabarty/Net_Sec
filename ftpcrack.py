c21deb9c384068aac0cb39d52c56474e1b5619f1
DiptoChakrabarty <diptochuck123@gmail.com>
2020-02-08 15:51:26 +0530

    send data using sockets

--- ftpcrack.py
+++ ftpcrack.py
@@ -8,3 +8,9 @@ def connection(ip,user,passwd):
     sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
     print("Connecting to ip" + ip  +"with user " + user + "password " +passwd)
+
+    sock.connect(('192.168.0.1',21))
+
+    data=sock.recv(1024)
+
+    
