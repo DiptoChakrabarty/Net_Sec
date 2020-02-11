8358a24a26998c9dfee5041590863aee6f8c24d4
DiptoChakrabarty <diptochuck123@gmail.com>
2020-02-11 22:48:22 +0530

    make mac addr human readable

--- packetsniff.py
+++ packetsniff.py
@@ -5,4 +5,10 @@ import textwrap
 #Unpack etehrnet packet
 
 def ether_frame(data):
-    dest_mac,src_mac,proto=struct.unpack('!',)
+    dest_mac,src_mac,proto=struct.unpack('! 6s 6s H',data[:14])
+    return get_mac_addr(dest_mac),get_mac_addr(src_mac),socket.htons(proto),data[:14]
+# Obtain Mac Addresses from data 
+
+# Make addresses human readable format
+def get_mac_addr(bytes_addr):
+    bytes_str= map()
