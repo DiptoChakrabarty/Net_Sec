951dd50f8f0c5b8ca6635936a884f6da30cb255a
DiptoChakrabarty <diptochuck123@gmail.com>
2020-02-12 20:35:40 +0530

    ipv4 addr conversion

--- packetsniff.py
+++ packetsniff.py
@@ -34,6 +34,11 @@ def unpack_packets(data):
     version=version_header_length >> 4
     header_length = (version_header_length & 15)*4
     ttl,proto,src,target = struct.unpack('! 8x B B 2x 4s 4s',data[:20])
+    return version,header_length,ttl,proto,ipv4(src),ipv4(target),data[header_length:]
 
+#Returns formatted ipv4 addresses
+
+def ipv4(addr):
+    return '.'.join(map(str,addr))
 
 packet_capture()
